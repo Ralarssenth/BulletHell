4c1ff2b116e6bb092d15f0d6b3541958f8e739ca
Ralarssenth <lamacleod23@gmail.com>
2024-05-20 17:32:47 -0400

    dummy damage taken
    -added a dps meter to the dummy scene that calculates the dps the dummy has taken over the last 5 seconds

--- scripts/dummy.gd
+++ scripts/dummy.gd
@@ -3,10 +3,15 @@ extends Area2D
 var max_health = 100.0
 var current_health = 100.0
 
+var dps:float
+var dps_health:float
+var dps_remainder:float = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CollisionShape2D/TextureProgressBar.max_value = max_health
 	$CollisionShape2D/TextureProgressBar.value = current_health
+	dps_health = max_health
 
 
 func _on_area_entered(area):
@@ -23,9 +28,12 @@ func damaged(_amount):
 	Globals.emit_signal("boss_damaged", current_health, max_health)
 	
 	if current_health <= 0:
+		dps_remainder = (dps_health - current_health) / 5.0
+		dps_health = max_health
+		
 		current_health = max_health
 		$CollisionShape2D/TextureProgressBar.value = current_health
-
+	
 
 func targeted(on):
 	$BossSprite2D/TargetCircle.set_visible(on)
@@ -33,3 +41,14 @@ func targeted(on):
 		$AnimationPlayer.play("targeted")
 	else:
 		$AnimationPlayer.stop()
+
+
+func _on_dps_timer_timeout():
+	dps = (dps_health - current_health) / (5.0)
+	dps_health = current_health
+	
+	if dps_remainder != 0.0:
+		dps += dps_remainder
+		dps_remainder = 0.0
+		
+	$DPSLabel.text = "DPS: " + str(dps)
