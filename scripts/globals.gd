2be859251679619bd75166c7b1fa31424bee5be2
Ralarssenth <lamacleod23@gmail.com>
2024-06-07 13:02:02 -0400

    Revert "steam integration"
    This reverts commit 7580d645da2feda117b95505bcbc97d068200418.

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -13,34 +13,15 @@ signal player_died
 signal update_player_target
 signal boss_damaged(current_health, max_health)
 
-#Steam vars
-var steam_id: int = 0
-var steam_username: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initialize_steam()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	Steam.run_callbacks()
+	pass
 
-func initialize_steam():
-	var initialize_response: Dictionary = Steam.steamInitEx()
-	print("Did Steam initialize?: %s " % initialize_response)
-	
-#	status
-#0 - Successfully initialized
-#1 - Some other failure
-#2 - We cannot connect to Steam, steam probably isn't running
-#3 - Steam client appears to be out of date
-	
-	if initialize_response['status'] > 0:
-		print("Failed to initialize Steam, shutting down: %s" % initialize_response)
-		get_tree().quit()
-	
-	# Gather additional data
-	steam_id = Steam.getSteamID()
-	steam_username = Steam.getPersonaName()
-	print("logged in to steam as: " + steam_username)
+
+
